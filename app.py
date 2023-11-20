41b7dc7b763bd8b48e96086eaf3605b234c964f1
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-11-20 16:01:35 +0200

    Implement backend for adding a new reference

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///reftool"
+db = SQLAlchemy(app)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    title = request.form["title"]
+    author = request.form["author"]
+    year = request.form["year"]
+    sql = "INSERT INTO Books (title, author, year) VALUES (:title, :author, :year)"
+    db.session.execute(text(sql), {"title": title, "author": author, "year": year})
+    db.session.commit()
+    return redirect("/")
