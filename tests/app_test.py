4721a227a18a7f2b7aa9c343645540f70492ba5d
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-11-22 18:27:55 +0200

    Add unit test to test that newly created reference actually appears on database

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,3 +1,20 @@
+from app.app import app
+from app.app import db
+from sqlalchemy.sql import text
+
 def test_opening_ref_creation_page(client):
     response = client.get("/new")
     assert b"Author" in response.data
+
+def test_created_ref_is_in_database(client):
+    with app.app_context():
+        db.session.execute(text("TRUNCATE TABLE books"))
+        db.session.commit()
+        response = client.post("/create", data={
+            "title": "Jotain",
+            "author": "Joku",
+            "year": "2002"
+        })
+        result = db.session.execute(text("SELECT * FROM books"))
+    ref = result.fetchone()
+    assert ref[1] == "Jotain" and ref[2] == "Joku" and ref[3] == 2002
