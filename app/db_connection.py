37554678acc7f9a09979b9a612a23805dc573b70
Janne <48507101+janneko01@users.noreply.github.com>
2023-11-23 23:46:28 +0200

    DB connection for Azure

--- app/db_connection.py
+++ app/db_connection.py
@@ -1,6 +1,16 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+import os
 
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=conn_str_params['user'],
+    dbpass=conn_str_params['password'],
+    dbhost=conn_str_params['host'],
+    dbname=conn_str_params['dbname']
+)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
