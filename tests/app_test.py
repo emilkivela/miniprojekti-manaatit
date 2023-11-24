1886be297e6ddcebbbbc7e7d5d68b6b284193961
Emil Kivelä <emil.kivela@gmail.com>
2023-11-24 10:32:37 +0200

    Add test to ref fetching

--- tests/app_test.py
+++ tests/app_test.py
@@ -19,3 +19,13 @@ def test_created_ref_is_in_database(client):
         result = db.session.execute(text("SELECT * FROM books"))
     ref = result.fetchone()
     assert ref[1] == "MIKA" and ref[2] == "Jotain" and ref[3] == "Joku" and ref[4] == 2002
+
+def test_index_fetches_refs_from_database(client):
+    with app.app_context():
+        db.session.execute(text("TRUNCATE TABLE books"))
+        db.session.commit()
+        sql = "INSERT INTO books (refkey, title, author, pubYear) VALUES (:key, :title, :author, :year)"
+        db.session.execute(text(sql), {"key": "MIKA", "title": "Sivullinen", "author": "Camus", "year": 1942})
+        db.session.commit()
+    response = client.get("/")
+    assert b"Sivullinen" and b"Camus" and b"1942" in response.data
