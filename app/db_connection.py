3197ac779a2d087bac874c9358a88a43eaabcfa5
Janne <48507101+janneko01@users.noreply.github.com>
2023-11-29 17:52:00 +0200

    Fix DB connection

--- app/db_connection.py
+++ app/db_connection.py
@@ -3,27 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_migrate import Migrate
 
-conn_str = os.environ["AZURE_POSTGRESQL_CONNECTIONSTRING"]
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 print(conn_str[:10])
-conn_str_params = {pair.split("=")[0]: pair.split("=")[1] for pair in conn_str.split(" ")}
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
-if "WEBSITE_HOSTNAME" not in os.environ:
-    DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'],
-    dbpass=os.environ['DBPASS'],
-    dbhost=os.environ['DBHOST'],
-    dbname=os.environ['DBNAME']
-    )
-
-else:
-    print("Loading production environment variables")
-    DATABASE_URI = "postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
-        dbuser=conn_str_params["user"],
-        dbpass=conn_str_params["password"],
-        dbhost=conn_str_params["host"],
-        dbname=conn_str_params["dbname"]
-    )
-
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=conn_str_params['user'],
+    dbpass=conn_str_params['password'],
+    dbhost=conn_str_params['host'],
+    dbname=conn_str_params['dbname']
+)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 db = SQLAlchemy(app)
 
