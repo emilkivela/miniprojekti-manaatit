ffd77aecb0240db8ba12540075ca45a19275487c
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-12-05 20:47:47 +0200

    Change run_robot_tests.sh to use gunicorn and add timeout for the gunicorn server startup

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# käynnistetään Flask-palvelin taustalle
-flask -A app/app run &> /dev/null &
+# käynnistetään gunicorn-palvelin taustalle
+gunicorn --bind=0.0.0.0 --timeout 600 startup:app &
 
-# odetetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:5000/ping)" != "404" ]];
+# odotetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
+i=0
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 0.0.0.0:8000/ping)" != "404" ]];
   do sleep 1;
+  ((i++))
+  if [ $i -gt 60 ]
+  then
+    exit 1
+  fi
 done
 
 # suoritetaan testit
-robot tests
+robot --variable SERVER:0.0.0.0:8000 tests
 
 status=$?
 
-# pysäytetään Flask-palvelin portissa 5000
-kill $(lsof -t -i:5000)
+# pysäytetään gunicorn-palvelin portissa 8000
+kill $(lsof -t -i:8000)
 
 exit $status
