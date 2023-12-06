c07f12c60267ae45dd4bf9a1a3d4b08f5d09ebf3
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-12-06 19:43:09 +0200

    Make run_robot_tests.sh stop gunicorn server when the server can't be reached

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -5,11 +5,17 @@ gunicorn --bind=0.0.0.0 --timeout 600 startup:app &
 
 # odotetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
 i=0
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 0.0.0.0:8000/ping)" != "404" ]];
-  do sleep 1;
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 0.0.0.0:8000/ping)" != "404" ]]
+do
+  sleep 1
   ((i++))
-  if [ $i -gt 60 ]
+  if [[ $i -gt 60 ]]
   then
+    process=$(lsof -t -i:8000)
+    if [[ $process ]]
+    then
+      kill $process
+    fi
     exit 1
   fi
 done
@@ -20,6 +26,10 @@ robot --variable SERVER:0.0.0.0:8000 tests
 status=$?
 
 # pysäytetään gunicorn-palvelin portissa 8000
-kill $(lsof -t -i:8000)
+process=$(lsof -t -i:8000)
+if [[ $process ]]
+then
+  kill $process
+fi
 
 exit $status
