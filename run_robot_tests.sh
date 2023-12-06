348c352eb58a940d2d209a1dbcf712cbe52e06e1
Ville Sirviö <VSirvio@users.noreply.github.com>
2023-12-06 20:21:39 +0200

    Make run_robot_tests.sh stop the gunicorn main process instead of the worker process

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
+SERVER_ADDRESS=0.0.0.0:8000
+
 # käynnistetään gunicorn-palvelin taustalle
-gunicorn --bind=0.0.0.0 --timeout 600 startup:app &
+gunicorn --bind=$SERVER_ADDRESS --timeout 600 startup:app &
+gunicorn_pid=$!
 
 # odotetaan, että palvelin on valmiina ottamaan vastaan pyyntöjä
 i=0
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 0.0.0.0:8000/ping)" != "404" ]]
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $SERVER_ADDRESS/ping)" != "404" ]]
 do
   sleep 1
   ((i++))
   if [[ $i -gt 60 ]]
   then
-    process=$(lsof -t -i:8000)
-    if [[ $process ]]
+    if [[ $(ps --no-headers -p $gunicorn_pid) ]]
     then
-      kill $process
+      kill $gunicorn_pid
     fi
     exit 1
   fi
 done
 
 # suoritetaan testit
-robot --variable SERVER:0.0.0.0:8000 tests
+robot --variable SERVER:$SERVER_ADDRESS tests
 
 status=$?
 
-# pysäytetään gunicorn-palvelin portissa 8000
-process=$(lsof -t -i:8000)
-if [[ $process ]]
+# pysäytetään gunicorn-palvelin
+if [[ $(ps --no-headers -p $gunicorn_pid) ]]
 then
-  kill $process
+  kill $gunicorn_pid
 fi
 
 exit $status
