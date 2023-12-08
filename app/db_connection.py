4645dedfb213b2430af61b1799153405f9fc2789
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2023-12-08 09:38:59 +0200

    Add user_id to db tables

--- app/db_connection.py
+++ app/db_connection.py
@@ -25,6 +25,7 @@ class Books(db.Model):
     author = db.Column(db.String(200), nullable=True)
     pubyear = db.Column(db.Integer, nullable=True)
     publisher = db.Column(db.String(200), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class Articles(db.Model):
     __tablename__= "articles"
@@ -36,12 +37,15 @@ class Articles(db.Model):
     pubyear = db.Column(db.Integer, nullable=True)
     volume = db.Column(db.String(200), nullable=True)
     pages = db.Column(db.String(200), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class Users(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), nullable=True)
     password = db.Column(db.String(200), nullable=True)
+    books = db.relationship('Books', backref='users')
+    articles = db.relationship('Articles', backref='users')
 
 with app.app_context():
     db.create_all()
